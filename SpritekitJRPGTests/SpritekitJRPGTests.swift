f30bbb8f92f152bd03254aa28377937f5e19b02e
Francisco Soares <fmssn@cin.ufpe.br>
2015-10-06 00:31:51 -0300

    Initial Commit

---
+++ SpritekitJRPGTests/SpritekitJRPGTests.swift
@@ -0,0 +1,36 @@
+//
+//  SpritekitJRPGTests.swift
+//  SpritekitJRPGTests
+//
+//  Created by Francisco Soares on 10/6/15.
+//  Copyright © 2015 frsoares. All rights reserved.
+//
+
+import XCTest
+@testable import SpritekitJRPG
+
+class SpritekitJRPGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
